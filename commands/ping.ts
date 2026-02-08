926f86b1f3c2b29f38909f1c9b42e7d60897eecd
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2026-02-08 19:54:08 +0800

    chore: component v2 implementation

--- commands/ping.ts
+++ commands/ping.ts
@@ -3,19 +3,37 @@ import {
   InteractionResponseTypes,
 } from "@discordeno/bot";
 import { Command } from "./mod.ts";
+import { logger } from "../utils/logger.ts";
+import { ComponentV2Type, IS_COMPONENTS_V2 } from "../utils/components_v2.ts";
 
 export const pingCommand: Command = {
   name: "ping",
-  description: "Replies with Pong!",
+  description: "Replies with Pong! (Components V2)",
   type: ApplicationCommandTypes.ChatInput,
   execute: async (bot, interaction) => {
+    logger.debug("Sending Pong response (V2) to {user}", {
+      user: interaction.user?.username ?? interaction.member?.user?.username ??
+        "Unknown",
+    });
+
     await bot.helpers.sendInteractionResponse(
       interaction.id,
       interaction.token,
       {
         type: InteractionResponseTypes.ChannelMessageWithSource,
         data: {
-          content: "Pong!",
+          flags: IS_COMPONENTS_V2,
+          components: [
+            {
+              type: ComponentV2Type.Container,
+              components: [
+                {
+                  type: ComponentV2Type.TextDisplay,
+                  content: "🏓 Pong!",
+                },
+              ],
+            },
+          ] as unknown as import("@discordeno/bot").ActionRow[],
         },
       },
     );
---
+++ utils/components_v2.ts
@@ -0,0 +1,33 @@
+/**
+ * Constants and helpers for Discord's Components V2 (introduced March 2025).
+ */
+
+/**
+ * Message flag required to activate Components V2 for a message.
+ * Setting this disables traditional 'content' and 'embeds' in favor of a component-only layout.
+ */
+export const IS_COMPONENTS_V2 = 1 << 15;
+
+/**
+ * Official Discord Component Type IDs for V2 System.
+ * Reference: https://discord.com/developers/docs/interactions/message-components#component-types
+ */
+export enum ComponentV2Type {
+  ActionRow = 1,
+  Button = 2,
+  StringSelect = 3,
+  TextInput = 4,
+  UserSelect = 5,
+  RoleSelect = 6,
+  MentionableSelect = 7,
+  ChannelSelect = 8,
+  Section = 9,
+  TextDisplay = 10,
+  Thumbnail = 11,
+  MediaGallery = 12,
+  File = 13,
+  Separator = 14,
+  Container = 17,
+  Label = 18,
+  File_Upload = 19,
+}
