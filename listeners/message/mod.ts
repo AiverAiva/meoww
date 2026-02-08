f9f2beb1adcf98dbe146a50867594bab9150b1a6
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2026-02-08 23:41:28 +0800

    feat: twitter preview

--- listeners/message/mod.ts
+++ listeners/message/mod.ts
@@ -1,8 +1,10 @@
 import { MessageListener } from "../../types.ts";
 import { helloListener } from "./hello.ts";
 import { pornhubListener } from "./pornhub.ts";
+import { twitterListener } from "./twitter.ts";
 
 export const messageListeners: MessageListener[] = [
   helloListener,
   pornhubListener,
+  twitterListener,
 ];
--- utils/components_v2.ts
+++ utils/components_v2.ts
@@ -12,6 +12,8 @@ export const IS_COMPONENTS_V2 = 1 << 15;
  * Official Discord Component Type IDs for V2 System.
  * Reference: https://discord.com/developers/docs/interactions/message-components#component-types
  */
+// Based on Discord documentation for Component V2 types
+// Note: Discordeno v21 might not have these updated enums yet.
 export enum ComponentV2Type {
   ActionRow = 1,
   Button = 2,
@@ -29,5 +31,5 @@ export enum ComponentV2Type {
   Separator = 14,
   Container = 17,
   Label = 18,
-  File_Upload = 19,
+  // File_Upload = 19
 }
